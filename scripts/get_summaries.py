169867f6245a25ec5ad7b7c0fa0057e6c6286315
Roberto Andrade <roberto.am1488@gmail.com>
2023-02-28 21:35:29 -0500

    Parallel processing and time of execution

--- scripts/get_summaries.py
+++ scripts/get_summaries.py
@@ -1,3 +1,5 @@
+import multiprocessing as mp
+import time
 import os
 os.environ['PYTORCH_CUDA_ALLOC_CONF'] = '5120'
 import torch
@@ -5,29 +7,53 @@ from get_plot import *
 from  load_model import *
 
 if __name__ == '__main__':
-    season_one_episodes = get_season_plot(season_one)
-    season_two_episodes = get_season_plot(season_two)
+    mp.set_start_method('spawn')
 
-    summaries_1 = []
-    for episode in season_one_episodes:
-        summary = make_episode_summary(episode)
-        summaries_1.append(summary)
-        torch.cuda.empty_cache()
-        gc.collect()
+    start = time.time()
+    if torch.cuda.device_count() > 1:
+        with mp.Pool(torch.cuda.device_count()) as pool:
+            summaries_1 = []
+            summaries_2 = []
+            summaries_boba = []
 
-    summaries_2 = []
-    for episode in season_two_episodes:
-        summary = make_episode_summary(episode)
-        summaries_2.append(summary)
-        torch.cuda.empty_cache()
-        gc.collect()
+            for summary in pool.map(make_episode_summary, season_one_episodes):
+                summaries_1.append(summary)
+                torch.cuda.empty_cache()
+                gc.collect()
 
-    summaries_boba = []
-    for episode in boba_fett_episodes:
-        summary = make_episode_summary(episode)
-        summaries_boba.append(summary)
-        torch.cuda.empty_cache()
-        gc.collect()    
+            for summary in pool.map(make_episode_summary, season_two_episodes):
+                summaries_2.append(summary)
+                torch.cuda.empty_cache()
+                gc.collect()
+
+            for summary in pool.map(make_episode_summary, boba_fett_episodes):
+                summaries_boba.append(summary)
+                torch.cuda.empty_cache()
+                gc.collect()
+
+    else:
+        summaries_1 = []
+        for episode in season_one_episodes:
+            summary = make_episode_summary(episode)
+            summaries_1.append(summary)
+            torch.cuda.empty_cache()
+            gc.collect()
+
+        summaries_2 = []
+        for episode in season_two_episodes:
+            summary = make_episode_summary(episode)
+            summaries_2.append(summary)
+            torch.cuda.empty_cache()
+            gc.collect()
+
+        summaries_boba = []
+        for episode in boba_fett_episodes:
+            summary = make_episode_summary(episode)
+            summaries_boba.append(summary)
+            torch.cuda.empty_cache()
+            gc.collect()         
+ 
+    end = time.time()
 
     try:
         os.makedirs('../output')
@@ -38,14 +64,14 @@ if __name__ == '__main__':
     with open('../output/summaries.md', 'w') as f:
         f.write('# **Season 1** \n\n')
         for i,episode_summary in enumerate(summaries_1):
-            f.write("### Episode "+str(i+1)+":\n")
+            f.write("### Chapter "+str(i+1)+":\n")
             f.write(episode_summary)
             f.write('\n\n')
 
     with open('../output/summaries.md', 'a') as f:
         f.write('\n# **Season 2** \n\n')
         for i, episode_summary in enumerate(summaries_2):
-            f.write("### Episode "+str(i+1+8)+":\n")
+            f.write("### Chapter "+str(i+1+8)+":\n")
             f.write(episode_summary)
             f.write('\n\n')
 
@@ -55,3 +81,7 @@ if __name__ == '__main__':
             f.write("### Episode "+str(i+1)+":\n")
             f.write(episode_summary)
             f.write('\n\n')
+
+    with open('../output/time_execution.txt','w') as f:
+        s = "Total time of execution: " + str(round((end-start)/60,2)) + " minutes."
+        f.write(s)
